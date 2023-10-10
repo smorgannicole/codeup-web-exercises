babef131b386d6d737ba8cd16a6bf7be6d2688bd
Morgan <smorgannicole@gmail.com>
2023-10-10 16:20:11 -0500

    completed two loop practice problems

--- js/loops-lecture.js
+++ js/loops-lecture.js
@@ -1,4 +1,4 @@
-console.log("loops");
+// console.log("loops");
 //meant for repeating blocks of code multiple times (usually arrays)
 
 //For loop
@@ -63,7 +63,7 @@ console.log("loops");
 //     console.log(`the current index is ${i}`);
 // }
 //
-// Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+// #1 Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
 //
 // For example, showMultiplicationTable(7) should output
 //
@@ -77,3 +77,33 @@ console.log("loops");
 // 7 x 8 = 56
 // 7 x 9 = 63
 // 7 x 10 = 70
+
+// const showMultiplicationTable = (enterNumber) => {
+//     let multipleResult;
+//     for(let i = 1; i <= 10; i++) {
+//         multipleResult = enterNumber * i;
+//         console.log(`${enterNumber} * ${i} = ${multipleResult}`);
+//     }
+//     return multipleResult;
+// }
+// let getThemMultiples = showMultiplicationTable(6);
+
+//#2 Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
+//
+//     123 is odd
+// 80 is even
+// 24 is even
+// 199 is odd
+// ...
+
+const oddOrEven = () => {
+    for(let i = 1; i <= 10; i++) {
+        const randomNumberGenerator = Math.floor(Math.random() * (200 - 20) + 20);
+        if(randomNumberGenerator % 2 === 0) {
+            console.log(`${randomNumberGenerator} is even`)
+        } else {
+            console.log(`${randomNumberGenerator} is odd`)
+        }
+    }
+}
+oddOrEven()
