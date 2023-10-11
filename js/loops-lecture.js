8e6799631ba654f1503108836a6aaa25b99af164
Morgan <smorgannicole@gmail.com>
2023-10-11 10:55:24 -0500

    solved ice cream problem!!!!!

--- js/loops-lecture.js
+++ js/loops-lecture.js
@@ -96,14 +96,45 @@
 // 199 is odd
 // ...
 
-const oddOrEven = () => {
-    for(let i = 1; i <= 10; i++) {
-        const randomNumberGenerator = Math.floor(Math.random() * (200 - 20) + 20);
-        if(randomNumberGenerator % 2 === 0) {
-            console.log(`${randomNumberGenerator} is even`)
-        } else {
-            console.log(`${randomNumberGenerator} is odd`)
-        }
+// const oddOrEven = () => {
+//     for(let i = 1; i <= 10; i++) {
+//         const randomNumberGenerator = Math.floor(Math.random() * (200 - 20) + 20);
+//         if(randomNumberGenerator % 2 === 0) {
+//             console.log(`${randomNumberGenerator} is even`)
+//         } else {
+//             console.log(`${randomNumberGenerator} is odd`)
+//         }
+//     }
+// }
+// oddOrEven()
+
+
+// An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. The below code shows how to get the random numbers for this exercise.
+//
+// // This is how you get a random number between 50 and 100
+// let allCones = Math.floor(Math.random() * 50) + 50;
+// // This expression will generate a random number between 1 and 5
+// Math.floor(Math.random() * 5) + 1;
+// The output should be similar to the following:
+//
+//     5 cones sold...  // if there are enough cones
+// Cannot sell you 6 cones I only have 3...  // If there are not enough cones
+// Yay! I sold them all! // If there are no more cones
+
+let allCones = Math.floor(Math.random() * 50) + 50;
+do {
+    let cones1To5 = Math.floor(Math.random() * 5) + 1;
+    let conesAlreadyBought = cones1To5++;
+    allCones = allCones - conesAlreadyBought;
+
+    if(allCones > conesAlreadyBought) {
+        console.log(`${cones1To5} cones sold.`)
+    } else if(allCones < conesAlreadyBought && allCones > 0) {
+        console.log(`I can't sell you ${cones1To5}, only have ${allCones} left. I can sell you ${allCones}`);
+    } else {
+        console.log("Yay I sold them all!");
     }
-}
-oddOrEven()
+} while (allCones >= 0)
+
+
+
