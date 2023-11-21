9690e9d5f6f215e333087c09d7375041e7e12900
Morgan <smorgannicole@gmail.com>
2023-11-21 10:50:54 -0600

    added async to our main

--- pokemon-starter/js/main.js
+++ pokemon-starter/js/main.js
@@ -1,25 +1,6 @@
-import {getPokemon} from "./pokemon-api.js";
+import {getPokemon, getPokemonAll} from "./pokemon-api.js";
 
-const getPokemonAll = async (offset = 0, limit = 10) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
-    const options = {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    };
-    const response = await fetch(url, options);
-    let list = await response.json();
-    const pokemon = await Promise.all(list.results.map((item) => getPokemon(item.name)));
-    list.results = pokemon;
-    return list;
-};
-
-(() => {
-    // getPokemon("munkidori").then((pokemon) => {
-    //     console.log(pokemon);
-    // });
-    getPokemonAll().then(list => {
-        console.log(list)
-    })
+(async () => {
+    const list = await getPokemonAll();
+    console.log(list);
 })();
