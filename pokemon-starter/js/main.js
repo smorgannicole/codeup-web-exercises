862d7337d6cd171f13cdf60f750c205c04ee0f71
Morgan <smorgannicole@gmail.com>
2023-11-21 10:22:41 -0600

    before async

--- pokemon-starter/js/main.js
+++ pokemon-starter/js/main.js
@@ -1,6 +1,23 @@
-import {getPokemon, getPokemonAll} from "./pokemon-api.js";
+import {getPokemon} from "./pokemon-api.js";
 
-(async () => {
-    const list = await getPokemonAll();
-    console.log(list);
+const getPokemonAll = async (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+    return fetch(url, options)
+        .then(response => response.json())
+        .catch((error) => console.log(error));
+}
+
+(() => {
+    // getPokemon("munkidori").then((pokemon) => {
+    //     console.log(pokemon);
+    // });
+    getPokemonAll().then(list => {
+        console.log(list)
+    })
 })();
--- pokemon-starter/js/pokemon-api.js
+++ pokemon-starter/js/pokemon-api.js
@@ -10,22 +10,3 @@ export const getPokemon = (id) => {
         .then((response) => response.json())
         .catch((error) => console.log(error));
 }
-
-export const getPokemonAll = async (offset = 0, limit = 10) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
-    const options = {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    };
-    try {
-        const response = await fetch(url, options);
-        let list = await response.json();
-        const pokemon = await Promise.all(list.results.map((item) => getPokemon(item.name)));
-        list.results = pokemon;
-        return list;
-    } catch (error) {
-        return error;
-    }
-};
